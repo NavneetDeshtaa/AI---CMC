d072ad85dd467d26f8b5b38942641a6920c4ea7c
Navneet Deshta <navneetdeshtaa@gmail.com>
2026-08-10 00:08:07 +0530

    Added API Points for the Contract Summary.

--- frontend/src/api/contracts.ts
+++ frontend/src/api/contracts.ts
@@ -1,6 +1,7 @@
 import axiosInstance from "./axiosInstance";
 import type { Contract} from "../types/contract";
 import type { SearchResponse } from "../types/search";
+import type { ContractSummary } from "../types/summary";
 
 export async function getContracts(): Promise<Contract[]> {
   const response = await axiosInstance.get<Contract[]>("/contracts");
@@ -21,7 +22,21 @@ export async function uploadContract(file: File): Promise<Contract> {
   return response.data;
 }
 
+
+// Search API functions :
 export async function searchContracts(query: string): Promise<SearchResponse> {
   const response = await axiosInstance.post<SearchResponse>("/search", { query });
+  return response.data;
+}
+
+
+// Summary API functions :
+export async function getSummary(contractId: string): Promise<ContractSummary> {
+  const response = await axiosInstance.get<ContractSummary>(`/contracts/${contractId}/summary`);
+  return response.data;
+}
+ 
+export async function regenerateSummary(contractId: string): Promise<ContractSummary> {
+  const response = await axiosInstance.post<ContractSummary>(`/contracts/${contractId}/summary/regenerate`);
   return response.data;
 }
